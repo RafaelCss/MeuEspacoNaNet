9e335dbf35189226003d3f7cf8c9a7a5bd1a0749
Rafael Carlos <rf2014souza@gmail.com>
2022-03-02 00:16:26 -0300

    Correções da pasta pages

--- pages/Projetos/index.tsx
+++ pages/Projetos/index.tsx
@@ -2,11 +2,7 @@ import React, { ReactElement } from "react";
 import Cabecalho from "../../Components/Header/Header";
 import { GetApi } from "../api/getapi";
 export default function Projetos(): ReactElement {
-  function dados() {
-    const response = GetApi;
-    console.log(response);
-  }
-  dados();
+
 
   return (
     <div>
