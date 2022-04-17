1c24ccd3e88e4567600ade996de3aba2c1db951d
Rafael carlos De souza <rf2014souza@gmail.com>
2022-04-17 20:45:01 -0300

    Cores

---
+++ public/Estilos/PaletaCores/Cores.ts
@@ -0,0 +1,13 @@
+export const paleta = [
+    {
+AçoAzul1	: '#63B8FF',
+AçoAzul2	:	'#5CACEE',
+AçoAzul3	:	'#4F94CD',
+AçoAzul4	:	'#36648B',
+AçoAzul5	:	'#2E5AA7',
+DeepSkyBlue1 :	'#00BFFF',
+DeepSkyBlue2	:	'#00B2EE',
+DeepSkyBlue3	:	'#009ACD',
+DeepSkyBlue4	:	'#00688B',
+    }
+]
