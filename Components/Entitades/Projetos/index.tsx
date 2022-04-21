369f4b7bfe1bb572eb968ba01f90a7e46f5a4087
Rafael carlos De souza <rf2014souza@gmail.com>
2022-04-20 22:15:39 -0300

    Ajustes home

--- Components/Entitades/Projetos/index.tsx
+++ Components/Entitades/Projetos/index.tsx
@@ -1,5 +1,5 @@
 import Menu from '../../Servicos/Header/Index'
-import Rodape from '../../Servicos/Footer/Index'
+import  Rodape  from '../../Servicos/Footer/Index'
 
 export default function PageProjetos(){
 
