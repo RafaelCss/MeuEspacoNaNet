8b16a8782b4a9477dba3f4b180b874025da61faa
Rafael carlos De souza <rf2014souza@gmail.com>
2022-04-24 03:04:31 -0300

    Animação projetos

--- Components/Entitades/Projetos/Apresentacao/Style.ts
+++ Components/Entitades/Projetos/Apresentacao/Style.ts
@@ -2,17 +2,17 @@ import styled from "styled-components";
 
 
 export const Fundo = styled.div`
-background-color: white;
-display: block;
+background-color: black;
+display:flex;
+justify-content: space-around;
 width: 100%;
 height: 100%;
 `
 
 
 export const Tijolos= styled.div`
-position: relative;
-background-color:${props=> props.color};
-width: ${props=> props.color};
+margin: 2px;
+padding: 1px;
 height: 20px;
-
+background-color: ${props=> props.color};
 `
--- Components/Entitades/Projetos/Apresentacao/index.tsx
+++ Components/Entitades/Projetos/Apresentacao/index.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from 'react'
 import { Fundo, Tijolos } from './Style'
+import cores from './Cores'
+import tijolos from './Cores'
 
 export default function Exibicao() {
-  return( 
-  <Fundo>
-    <Tijolos color='blue'/>
-  </Fundo>)
+  const [cor, setCor] = useState('')
+  const [tijolo, setTijolo] = useState('')
+  function mudarCor() {
+    let ix ;
+    for (let i = 0; i < cores.length; i++) {
+      let index = Math.floor(Math.random() * cores.length)
+      ix = cores[index]
+      setTijolo(`${index*5}px`)
+    }
+    setCor(ix)
+  }
+  useEffect(() => {
+    setInterval(() => {
+      mudarCor()
+    }, 1000)
+  }, [])
+
+  return (
+    <Fundo>
+      {tijolos.map((item) => {
+        return (
+          <Tijolos
+            key={item}
+            style={{ backgroundColor: `${cor}`, width: tijolo }}
+          />
+        )
+      })}
+    </Fundo>
+  )
 }
