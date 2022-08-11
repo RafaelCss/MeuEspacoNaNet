75af9f96c1f86ee3c4044b56e96c538e081fa872
devspacerj <rf2014souza@gmail.com>
2022-08-11 08:43:26 -0300

    Ajuste função de animação

--- Components/Entitades/Home/AnimacaoTexto/Index.tsx
+++ Components/Entitades/Home/AnimacaoTexto/Index.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import { BannerHome, TextCursor, Container } from "./Style";
 
 function Banner() {
-  const mensagem = "SEJA BEM VINDO !";
 
+  const [mensagem, setMensagem] = useState<string>("SEJA BEM VINDO !");
   const [texto, setTexto] = useState("");
 
-  const animacaoTexto = async (texto1: string, i: number = 0): Promise<void> => {
+  async function animacaoTexto  (texto1: string, i: number = 0): Promise<void>  {
     if (i < mensagem.length) {
       setTexto(texto1.slice(0, i + 1));
       setTimeout(() => {
@@ -18,11 +18,12 @@ function Banner() {
   useEffect(() => {
     animacaoTexto(mensagem);
     setTexto("");
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[mensagem]);
 
   return (
     <BannerHome>
-      <h1>Developer Web</h1>
+      <h1>Desenvolvedor Full-Stack</h1>
       <Container>
         {texto}
         <TextCursor/>
