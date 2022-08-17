8813a09d1630d7a5f27ef030a519d3ddc60dcfae
devspacerj <rf2014souza@gmail.com>
2022-08-17 08:52:19 -0300

    ajuste na home 17/08/2022

--- pages/Vitrine/style.ts
+++ pages/Vitrine/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 
-export const Container = styled.div`
+ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
