4c019311f13fde38503f1b73db041467f7389294
devspacerj <rf2014souza@gmail.com>
2023-02-11 20:56:00 -0300

    feature/11-02 Ainda atualizandoo front do site

--- src/Componentes/Button/ButtonNormal/style.tsx
+++ src/Componentes/Button/ButtonNormal/style.tsx
@@ -4,23 +4,23 @@ import styled from 'styled-components';
 const ButtonEnviar = styled(ButtonAntd)`
   width: 25rem;
   height: 5vh;
-  color: white;
+  color: #000000ef;
   border: none;
   -webkit-box-shadow: -11px -15px 40px 1px rgba(157, 247, 139, 0.45);
   -moz-box-shadow: -11px -15px 40px 1px rgba(149, 240, 131, 0.45);
   box-shadow: -11px -15px 40px 1px rgba(159, 240, 83, 0.753);
-  background-color: #00ff00;
+  background-color: #fbfdfb;
 `;
 
 const ButtonLimpar = styled(ButtonAntd)`
   width: 25rem;
   height: 5vh;
-  color: white;
+  color: #050404;
   border: none;
   -webkit-box-shadow: -11px -15px 40px 1px rgba(250, 107, 107, 0.45);
   -moz-box-shadow: -11px -15px 40px 1px rgba(250, 84, 54, 0.45);
   box-shadow: -11px -15px 40px 1px rgba(252, 101, 101, 0.753);
-  background-color: #ff1e0099;
+  background-color: #fcf7f6;
 `;
 
 export default { ButtonEnviar, ButtonLimpar };
